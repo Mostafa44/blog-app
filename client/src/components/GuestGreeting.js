03d4b33a2a0d1190e1dd03f95370e60ca1e716d9
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2021-07-03 23:33:35 +0200

    update the front end to have a message asking for logging in

---
+++ client/src/components/GuestGreeting.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const GuestGreeting = () => {
+
+    return <h1>Hello Guest , please log-in to be able to post</h1>
+}
+
+export default GuestGreeting
--- client/src/components/content.js
+++ client/src/components/content.js
@@ -1,18 +1,44 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 //import contentData from "../utils/content-data";
 //import { Col, Row } from "react-bootstrap";
 import PostCreate from './PostCreate';
 import PostList from './PostList'
+import GuestGreeting from "./GuestGreeting";
 
-const Content = () => (
-  <div className="container">
-    <h1>Create Post</h1>
-    <PostCreate />
-    <hr />
-    <h2>Posts</h2>
-    <PostList />
-  </div>
-);
+const Content = () => {
+  const { getIdTokenClaims, isAuthenticated, getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+
+    const getUserAccessToken = async () => {
+      const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+      try {
+        const accessToken = await getAccessTokenSilently({
+          // audience: `https://${domain}/`,
+          // scope: "read:current_user",
+        });
+        console.log('Access token: ', accessToken);
+        const claims = await getIdTokenClaims();
+        console.log('Id token: ', claims.__raw);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    getUserAccessToken();
+  }, [isAuthenticated]);
+
+  if (isAuthenticated) {
+    return (<div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h2>Posts</h2>
+      <PostList />
+    </div>);
+  } else {
+    return <GuestGreeting />
+  }
+};
 
 export default Content;
