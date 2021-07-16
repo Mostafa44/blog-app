95fdb5ebcf4e382183f5b2fe83ba297deec95704
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2021-07-16 23:29:33 +0200

    rename notfound component

--- client/src/components/not-found.js
+++ client/src/components/not-found.js
@@ -1,7 +1,7 @@
-import React from 'react';
-const NotFound= ()=>{
+import React from "react";
+const NotFound= () => {
     return (
         <h1>Not Found</h1>
-    )
+    );
 }
 export default NotFound;
