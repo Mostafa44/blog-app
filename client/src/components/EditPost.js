7eb19fd806cfcf78c5cdd342ec748e895c65c8f3
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2021-07-18 21:54:10 +0200

    Update the ui of the edit post component so that it has an uplaod file functionality

--- client/src/components/EditPost.js
+++ client/src/components/EditPost.js
@@ -1,15 +1,74 @@
-import React , {useContext} from 'react';
+import React , {useContext, useState} from 'react';
 import UserContext from '../context/userContext';
 import { Form, Button } from 'semantic-ui-react'
+import {getUploadUrl, uploadFile} from '../api/posts-apis';
  
 
-const EditPost=()=>{
+const EditPost=(props)=>{
     const userContext = useContext(UserContext);
-    return (
+    const {postId}= props.match.params;
+    const [file, setFile]= useState(undefined);
+    const [isUploading, setIsUploading]= useState(false);
+    const userIdToken=  userContext.idToken ? userContext.idToken : "";
+    const handleFileChange = (event) => {
+        const files = event.target.files
+        if (!files) return
+        setFile(files[0]);
+      }
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+    
+        try {
+          if (!file) {
+            alert('File should be selected')
+            return
+          }
+    
+         // this.setUploadState(UploadState.FetchingPresignedUrl)
+          const uploadUrl = await getUploadUrl(userIdToken, postId)
+    
+          setIsUploading(true);
+          await uploadFile(uploadUrl, file)
+    
+          alert('File was uploaded!')
+        } catch (e) {
+          alert('Could not upload a file: ' + e.message)
+        } finally {
+            setIsUploading(false);
+        }
+      }
+      const   renderButton=()=> {
+
+        return (
+          <div>
+            {isUploading && <p>Uploading file</p>}
+            <Button
+              loading={isUploading}
+              type="submit"
+            >
+              Upload
+            </Button>
+          </div>
+        )
+      }
+      return (
         <div>
-          Edit Post {userContext.idToken ? userContext.idToken : ""}
+          <h1>Upload new file</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Field>
+              <label>File</label>
+              <input
+                type="file"
+                accept="image/*"
+                placeholder="Image to upload"
+                onChange={handleFileChange}
+              />
+            </Form.Field>
+  
+            {renderButton()}
+          </Form>
         </div>
-      );
+      )
 }
 
 export default EditPost;
